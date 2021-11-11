ebb3b207bf66f29e223cd4963d091c461b74231c
Rodrigo Filho <rodrigofilhogt@gmail.com>
2021-11-11 13:39:15 -0300

    a

--- bd/supermercado_bd.sql
+++ bd/supermercado_bd.sql
@@ -1,57 +1,141 @@
 create database supermercado_bd;
 use supermercado_bd;
 
-create table produtos(
-cod_prod int unsigned auto_increment not null primary key,
-nome varchar(80) not null,
-cod_barras int not null,
-dt_fabr date not null,
-dt_venc date not null,
-lote int not null,
-quantidade int not null,
-foto varchar(115) not null,
-preco double(9,2) not null
-)engine=innoDB;
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`cliente` (
+  `cod_cliente` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(80) NOT NULL,
+  `cpf` CHAR(12) NOT NULL,
+  `email` VARCHAR(100) NULL,
+  `telefone` CHAR(14) NOT NULL,
+  `endereco` VARCHAR(200) NULL,
+  PRIMARY KEY (`cod_cliente`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-create table funcionario(
-cod_funcionario int unsigned auto_increment not null primary key,
-nome varchar(80) not null,
-cpf char(11) not null,
-telefone char(11) not null,
-email varchar(100) not null,
-senha varchar(16) not null
-)engine=innoDB;
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`funcionario` (
+  `cod_funcionario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `senha` VARCHAR(45) NOT NULL, 
+  `email` VARCHAR(100) NOT NULL,   
+  `nome` VARCHAR(80) NOT NULL,
+  `cpf` CHAR(12) NOT NULL,
+  `telefone` CHAR(14) NOT NULL,
+  `foto` VARCHAR(150) NOT NULL,  
+  PRIMARY KEY (`cod_funcionario`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-create table cliente(
-cod_cliente int unsigned auto_increment not null primary key,
-nome varchar(80) not null,
-cpf char(11) not null,
-email varchar(100) not null,
-telefone char(11) not null,
-endereco varchar(200) not null
-)engine=innoDB;
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`categoria` (
+  `cod_categoria` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`cod_categoria`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-create table venda(
-cod_venda int unsigned auto_increment not null primary key,
-valor double(9,2) not null,
-data date not null,
-cliente_cod_cliente int unsigned not null,
-produtos_cod_prod int unsigned not null,
-funcionario_cod_funcionario int unsigned not null,
-foreign key(cliente_cod_cliente) references cliente(cod_cliente),
-foreign key(produtos_cod_prod) references produtos(cod_prod),
-foreign key(funcionario_cod_funcionario) references funcionario(cod_funcionario)
-)engine=innoDB;
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`fornecedor` (
+  `cod_fornecedor` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `cnpj` INT NOT NULL,
+  PRIMARY KEY (`cod_fornecedor`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`produtos` (
+  `cod_prod` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `cod_barras` INT NOT NULL,
+  `dt_fabr` DATE NOT NULL,
+  `dt_venc` DATE NOT NULL,
+  `lote` INT NOT NULL,
+  `quantidade` INT NOT NULL,
+  `preco` DOUBLE(9,2) NOT NULL,
+  `foto` VARCHAR(115) NOT NULL,
+  `categoria_cod_categoria` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`cod_prod`),
+  CONSTRAINT `fk_produtos_categoria1`
+    FOREIGN KEY (`categoria_cod_categoria`)
+    REFERENCES `supermercado_bd`.`categoria` (`cod_categoria`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`carrinho` (
+  `codigo` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `cliente_cod_cliente` INT UNSIGNED NOT NULL,
+  `produtos_cod_prod` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`codigo`),
+  CONSTRAINT `fk_carrinho_cliente1`
+    FOREIGN KEY (`cliente_cod_cliente`)
+    REFERENCES `supermercado_bd`.`cliente` (`cod_cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_carrinho_produtos1`
+    FOREIGN KEY (`produtos_cod_prod`)
+    REFERENCES `supermercado_bd`.`produtos` (`cod_prod`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`venda` (
+  `cod_venda` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `valor` DOUBLE(9,2) NOT NULL,
+  `data` DATE NOT NULL,
+  `cliente_cod_cliente` INT UNSIGNED NOT NULL,
+  `produtos_cod_prod` INT UNSIGNED NOT NULL,
+  `funcionario_cod_funcionario` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`cod_venda`),
+  CONSTRAINT `fk_venda_cliente`
+    FOREIGN KEY (`cliente_cod_cliente`)
+    REFERENCES `supermercado_bd`.`cliente` (`cod_cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_venda_produtos1`
+    FOREIGN KEY (`produtos_cod_prod`)
+    REFERENCES `supermercado_bd`.`produtos` (`cod_prod`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_venda_funcionario1`
+    FOREIGN KEY (`funcionario_cod_funcionario`)
+    REFERENCES `supermercado_bd`.`funcionario` (`cod_funcionario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-drop database supermercado_bd;
+CREATE TABLE IF NOT EXISTS `supermercado_bd`.`fornecedor_has_produtos` (
+  `fornecedor_cod_fornecedor` INT UNSIGNED NOT NULL,
+  `produtos_cod_prod` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`fornecedor_cod_fornecedor`, `produtos_cod_prod`),
+  CONSTRAINT `fk_fornecedor_has_produtos_fornecedor1`
+    FOREIGN KEY (`fornecedor_cod_fornecedor`)
+    REFERENCES `supermercado_bd`.`fornecedor` (`cod_fornecedor`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_fornecedor_has_produtos_produtos1`
+    FOREIGN KEY (`produtos_cod_prod`)
+    REFERENCES `supermercado_bd`.`produtos` (`cod_prod`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
-
-
+drop table categoria;
+drop database `supermercado_bd`;
+delete from `produtos` where `cod_prod` = 3;
 select * from funcionario;
-select * from produtos;
+select * from `produtos`;
+select * from categoria;
+set sql_safe_updates = 0;
+update produtos set preco = preco * 1.20;
+select nome, email from funcionario;
+
+select * from funcionario where email = 'juliana@gmail.com' and senha = 1234;
+
+
+
+
+-- C:/Users/rodri/Downloads/painel_supermercado/painel_supermercado/Resources/login_icon.png
+
 
 
 
--- login/Conexao.php
+++ login/Conexao.php
@@ -35,17 +35,19 @@ class Conexao
         //executar selcet
     }public function executeSelect($comando){
     try {
-        $sql = $this -> conn -> prepare($comando);
+        $sql = $this->conn->prepare($comando);
         //executar o comando no servidor
-        $sql -> execute();
+        $sql->execute();
+
+        //var_dump($comando,$sql->rowCount()); die();
         //testar se deu certo
         if ($sql->rowCount() > 0){
             //fetch_all retorna tudo do selcet
             //fetch_class retornar dados na classe
             //fetch_assoc retrnar dados do vetor
-            return $sql->fetchAll(PDO::FETCH_CLASS); //retornar o comando para a tela
+           return $sql->fetchAll(PDO::FETCH_CLASS); //retornar o comando para a tela
         }else{ // se cair, deu erro na execucao do comando
-            return $sql->errorInfo(); //retornar o comando para a tela
+            return '0'; //retornar o comando para a tela
         }
     }catch (PDOException $msg){
         echo "Não foi possível executar o comando" . $msg -> getMessage();
@@ -60,7 +62,7 @@ class Conexao
             //criar uma instância da classe
             $bd = new Conexao();
             //criar uma variável para receber o comando insert
-            $sql = "select * from funcionario where email = '{this->EMAIL}' and senha = '{this->SENHA}'";
+            $sql = "select * from funcionario where email = '{$this->EMAIL}' and senha = '{$this->SENHA}'";
 
             return $bd->executeSelect($sql);
         }catch(PDOException $msg){
